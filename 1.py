8cdf913550e468d65afea624e05250cf99d37218
4fth4b <afthababid44@gmail.com>
2023-02-22 12:16:15 +0530

    push

---
+++ 1.py
@@ -0,0 +1,10 @@
+class computer:
+    def __init__(self,cpu,name):
+        self.cpu=cpu
+        self.name=name
+    def config(self):
+        print('the config is :',self.cpu,self.name)
+    def name1(self):
+        print('the name is :',self.name)
+com1=computer('i7','game1')
+com2=computer('ryzen7','game2')
---
+++ 3.py
@@ -0,0 +1,11 @@
+class person:
+    def __init__(self):
+        self.name='Afthab'
+        self.age=21
+    def update(self):
+        self.age=22
+        print(self.age)
+c1=person()
+c1.update()
+c1.name
+c1.age
